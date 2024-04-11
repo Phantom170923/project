c241e122776e3cab84fb427aff8c9d65889abb4d
Phantom170923 <164197993+Phantom170923@users.noreply.github.com>
2024-04-11 11:36:51 +0700

    Add files via upload

--- project.py
+++ project.py
@@ -1,97 +1,134 @@
 import os
-import pandas as pd
+import json
 
 
-class PriceMachine:
+class PriceMachine():
     
-    def __init__(self, request):
-        self.request = request
-        self.result = pd.DataFrame()
-        self.my_list = []
+    def __init__(self):
         self.data = []
+        self.result = ''
         self.name_length = 0
-        self.directory = 'C:\\Users\\User\\PycharmProjects\\Practic\\задание'
     
-    def load_prices(self):
+    def load_prices(self, file_path=''):
+        '''
+            Сканирует указанный каталог. Ищет файлы со словом price в названии.
+            В файле ищет столбцы с названием товара, ценой и весом.
+            Допустимые названия для столбца с товаром:
+                товар
+                название
+                наименование
+                продукт
+                
+            Допустимые названия для столбца с ценой:
+                розница
+                цена
+                
+            Допустимые названия для столбца с весом (в кг.)
+                вес
+                масса
+                фасовка
+        '''
+        count_files = 0
+        count_lines = 0
+        self.data = []
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        if not file_path:
+            file_path = current_path
+        for file_name in os.listdir(file_path):
+            if 'price' in file_name:
+                count_files += 1
+                print('read', file_name)
+                with open(os.path.join(file_path, file_name), 'r', encoding='utf-8') as f:
+                    headers = f.readline()
+                    product_name_number, price_number, weight_number = self._search_product_price_weight(headers)
+                    file_data = f.readlines()
+                    for line in file_data:
+                        count_lines += 1
+                        line_data = line.split(',')
+                        product_name = line_data[product_name_number].strip().lower()
+                        if len(product_name) > self.name_length:
+                            self.name_length = len(product_name)
+                        price = int(line_data[price_number].strip())
+                        weight = int(line_data[weight_number].strip())
+                        value = round(price / weight, 2)
+                        self.data.append((value, product_name, price, weight, file_name))
+        self.data.sort()
+        return count_files, count_lines
+        
+    def _search_product_price_weight(self, headers):
+        '''
+            Возвращает номера столбцов
+        '''
+        data = headers.lower().strip().split(',')
+        product_name_number = '!'
+        for index in range(len(data)):
+            if data[index] in ('товар','название','продукт','наименование'):
+                product_name_number = index
+            if data[index] in ('розница','цена'):
+                price_number = index
+            if data[index] in ('вес','масса','фасовка'):
+                weight_number = index
+        return product_name_number, price_number, weight_number
 
-        sp_name = []
-        sp_weight = []
-        sp_price = []
-        sp_price_kg = []
-        price_column_name = ["цена", "розница"]
-        name_column_name = ["название", "продукт", "товар", "наименование"]
-        weight_column_name = ["фасовка", "масса", "вес"]
-
-        for filenames in os.listdir(self.directory):
-            if 'price' in filenames:
-                df = pd.read_csv(filenames)
-
-                for i, row in df.iterrows():
-                    sp = []
-
-                    for name in price_column_name:
-                        if name in row:
-                            sp_price.append(row[name])
-                            sp.append(row[name])
-                    for name in name_column_name:
-                        if name in row:
-                            sp_name.append(row[name])
-                            sp.append(row[name])
-                    for name in weight_column_name:
-                        if name in row:
-                            sp_weight.append(row[name])
-                            sp.append(row[name])
-                    self.data.append(sp)
-            else:
-                continue
-
-        for i in self.data:
-            price_kg = int(i[0]) // int(i[2])
-            sp_price_kg.append(price_kg)
-
-        self.result = pd.DataFrame(
-            {
-                'цена': sp_price,
-                'наименование': sp_name,
-                'вес': sp_weight,
-                'цена за кг': sp_price_kg
-            }
-        )
-        self.result.sort_values(by='цена за кг', inplace=True, ascending=True)
-        self.result.reset_index(drop=True, inplace=True)
-        self.result.to_csv('my_result.csv', sep=';')
-        return self.result
-
-    def export_to_html(self):
-
-        return self.result.to_html('my_output.html', encoding='cp1251')
+    def export_to_html(self, fname='output.html'):
+        result = '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Позиции продуктов</title>
+        </head>
+        <body>
+            <table>
+                <tr>
+                    <th>Номер</th>
+                    <th>Название</th>
+                    <th>Цена</th>
+                    <th>Фасовка</th>
+                    <th>Файл</th>
+                    <th>Цена за кг.</th>
+                </tr>
+        '''
+        for number, item in enumerate(self.data):
+            value, product_name, price, weight, file_name = item
+            result += '<tr>' 
+            result += f'<td>{number + 1}</td>' 
+            result += f'<td>{product_name}</td>' 
+            result += f'<td>{price}</td>' 
+            result += f'<td>{weight}</td>' 
+            result += f'<td>{file_name}</td>' 
+            result += f'<td>{value}</td>' 
+            result += '</tr>\n' 
+        result += '</tbody>'
+        result += '</table>'
+        print(fname)
+        with open(fname, 'w') as f:
+            f.write(result)
+        return 'ok'
     
-    def find_text(self):
-
-        self.request = input('Введите запрос: ')
-
-        if self.request == 'exit':
-            return 'Работа окончена'
-
-        else:
-            self.result = pm.result[pm.result['наименование'].str.contains(self.request, case=False)]
-
-            self.result = self.result.copy()
-
-            self.result.sort_values(by='цена за кг', inplace=True, ascending=True)
-
-            self.result.reset_index(drop=True, inplace=True)
-
-            return self.result
+    def find_text(self, text):
+        text = text.lower()
+        data = [item for item in self.data if text in item[1]]
+        data.sort()
+        return data
 
     
 pm = PriceMachine()
-
 print(pm.load_prices())
-
-print(pm.find_text())
-
+while 1:
+    command = input('Введите exit для выхода или часть названия для поиска: \n')
+    if command == 'exit':
+        break
+    else:
+        name = command
+        res = pm.find_text(name)
+        print(f'{"№": <4}  {"Наименование": <{pm.name_length}} {"цена":^5} {"вес":^3} {"файл":^12} {"цена за кг."}')
+        for number, item in enumerate(res):
+            print(f'{number + 1: <4}  {item[1]: <{pm.name_length}} {item[2]:^5}  {item[3]:^3} {item[4]:^12} {item[0]}')
+print('the end')
 print(pm.export_to_html())
 
-print('the end')
 
+'''
+добавить в условие "цена за кг и сортировка"
+развитие задания: 
+'''
